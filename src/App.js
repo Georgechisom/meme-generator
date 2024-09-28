6842676d32ceafc9a4145ea4a4402979ce27b49a
George winsome <georgechipaul@gmail.com>
2024-09-28 20:04:37 +0100

    meme generator app installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
+
+
+{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +26,4 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+      </header> */}
